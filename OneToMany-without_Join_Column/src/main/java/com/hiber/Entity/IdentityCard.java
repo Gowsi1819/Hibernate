f980b537469adc8be315376b52b3ab3da40ee884
Gowsika R <gowsicreator@gmail.com>
2025-10-13 16:35:01 +0530

    Add files via upload

---
+++ OneToMany-without_Join_Column/src/main/java/com/hiber/Entity/IdentityCard.java
@@ -0,0 +1,64 @@
+package com.hiber.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="IdentityCard")
+public class IdentityCard{
+	
+	@Id
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="name")
+	private String  name;
+	
+	@Column(name="address")
+	private String address;
+
+	public IdentityCard() {
+		
+	}
+
+	public IdentityCard(int id, String name, String address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.address = address;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "Passport [id=" + id + ", name=" + name + ", address=" + address + "]";
+	}
+	
+	
+}
