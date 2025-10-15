b067d3618e270626a56a82f03042886938959bfd
Gowsika R <gowsicreator@gmail.com>
2025-10-15 10:58:07 +0530

    Add files via upload

---
+++ ManyToMany-Bidirectional/src/main/java/com/hiber/Launch/Launch.java
@@ -0,0 +1,38 @@
+package com.hiber.Launch;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.hiber.Entity.Employee;
+import com.hiber.Entity.Project;
+
+
+public class Launch {
+
+	public static void main(String args[]) {
+		Configuration cfg = new Configuration();
+		cfg.configure();
+		cfg.addAnnotatedClass(Employee.class);
+		cfg.addAnnotatedClass(Project.class);
+		
+		SessionFactory sessionfactory = cfg.buildSessionFactory();
+		Session session = sessionfactory.openSession();
+		Transaction t = session.beginTransaction();
+
+        
+		Project p=session.get(Project.class, 23);
+		
+		List<Employee> e=p.getEmployee();
+		
+		System.out.println(e);
+		System.out.println(p);
+		
+		
+		t.commit();
+	}
+}
